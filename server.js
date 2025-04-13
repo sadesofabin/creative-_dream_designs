ec9b6e6777e1bcc1f7c9e47febad3b0868423ec6
abin-mcabee <abinmathew@mcabee.in>
2025-04-14 02:32:38 +0530

    second commit

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.get('/health', (req, res) => {
+  res.send('OK');
+});
+
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
   port: 587,
